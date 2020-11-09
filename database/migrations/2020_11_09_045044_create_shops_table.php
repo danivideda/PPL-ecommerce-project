e3dd41888fc486d83f36c1ff8f37f8922c221043
Dani Ihza Farrosi <dani.farrosi@jakpat.net>
2020-11-09 14:30:34 +0700

    Create databases

---
+++ database/migrations/2020_11_09_045044_create_shops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->timestamp('create_time')->nullable();
+            $table->string('ratings', 200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
